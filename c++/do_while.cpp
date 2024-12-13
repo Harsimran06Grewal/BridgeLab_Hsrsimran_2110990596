5ab4e7d83899417d7b5afa9811a11cf5b03bc55c
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2024-12-13 11:39:08 +0530

    [Harsimran]add:adding file do_while

---
+++ c++/do_while.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     do{
+//         cout<<"Hello Word";
+//     }
+//     while(3>5);
+// }
+
+///print numbers upto n
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int n=10;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=n);
+
+}
