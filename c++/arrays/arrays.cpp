f8d12bf57f59234601d4762fc2eb1519c2433d95
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2024-12-17 15:21:25 +0530

    [Harsimran] add : adding arrays file

---
+++ c++/arrays/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int marks[5] = {99, 90, 100, 56, 98};
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+}
